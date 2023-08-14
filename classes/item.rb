4aeb9986b8ecb574e44eb38fd1c4a9516699a8cb
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-14 22:00:51 +0500

    Add methods to item

--- classes/item.rb
+++ classes/item.rb
@@ -1,5 +1,5 @@
 class Item
-  def initialize(id, genre, author, source, label, publish_date, archived: flase)
+  def initialize(id, genre, author, source, label, publish_date, archived: false)
     @id = Random.rand()
     @genre = genre
     @author = author
@@ -8,6 +8,19 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
+    def can_be_archived
+      current_date= Time.now
+      length_year = current_date - publish_date 
+      if length_year > 10
+        true
+      else 
+        false
+      end
+    end
 
+    def move_to_archived
+      can_be_archived? true
+       @archived = true
+    end
 
 end
