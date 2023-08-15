2b52892edd75f61e363a7a05e2fa504cbcce71c5
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-08-16 01:10:07 +0500

    Complete the perserve data and create JSON files

--- main.rb
+++ main.rb
@@ -1,64 +1,43 @@
 require_relative 'app'
-def menu
-  @options = [
-    '1 - List all books',
-    '2 - List all music albums',
-    '3 - List all movies',
-    '4 - List all games',
-    '5 - List all genres',
-    '6 - List all labels',
-    '7 - List all authors',
-    '8 - List all sources',
-    '9 - Add a book',
-    '10 - Add a music album',
-    '11 - Add a movie',
-    '12 - Add a game',
-    '13 - Exit the App'
-  ]
 
-  puts "\nPlease choose an option by entering a number:"
-  puts @options
-end
-
-CHOICES = {
-  1 => 'list_all_books',
-  2 => 'list_all_music_albums',
-  3 => 'list_all_movies',
-  4 => 'list_all_games',
-  5 => 'list_all_genres',
-  6 => 'list_all_labels',
-  7 => 'list_all_authors',
-  8 => 'list_all_sources',
-  9 => 'add_book',
-  10 => 'add_music_album',
-  11 => 'add_movie',
-  12 => 'add_game'
-}.freeze
-
-def option_case(choice, app)
-  case choice
-  when 1..12
-    app.send(CHOICES[choice])
+def display_options
+  options = { 1 => 'List books', 2 => 'List music albums', 3 => 'List games',
+              4 => 'List genres', 5 => 'List labels', 6 => 'List authors',
+              7 => 'Add a book', 8 => 'Add a music album',
+              9 => 'Add a game', 10 => 'Exit' }
+  options.each { |key, value| puts "#{key} - #{value}\n" }
+  choice = gets.chomp.to_i
+  if choice >= 1 && choice <= 10
+    choose_option(choice)
   else
-    puts 'Incorrect choice, choose a number between 1..12'
+    puts 'Please choose a valid option'
   end
 end
 
-def prompt_user(app)
-  menu
-  print 'Enter choice:'
-  choice = gets.chomp.to_i
-  choice == 13 ? app.quit_app : option_case(choice, app)
+def choose_option(choice)
+  list_all_books if choice == 1
+  list_all_music_albums if choice == 2
+  list_of_games if choice == 3
+  choose_option_one(choice)
+end
 
-  prompt_user(app)
+def choose_option_one(choice)
+  list_all_genres if choice == 4
+  list_all_labels if choice == 5
+  list_all_authors if choice == 6
+  choose_option_two(choice)
+end
+
+def choose_option_two(choice)
+  add_a_book if choice == 7
+  add_a_music_album if choice == 8
+  add_a_game if choice == 9
+  exit if choice == 10
 end
 
 def main
-  puts "\n* * * * * * * * * * * * * * * * * * *"
-  puts "\n*  Welcome to Catalog of my things App!   *"
-  puts "\n* * * * * * * * * * * * * * * * * * *"
   app = App.new
-  prompt_user(app)
+  app.run
 end
 
 main
